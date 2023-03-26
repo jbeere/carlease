31612876ce6ce9f2821aff320cb557cc92c0ecc7
Justin Beere <justin.beere@gmail.com>
2023-03-26 09:00:17 +0200

    use a real database

--- customer-api/src/main/java/com/carlease/customer/api/CustomerApiApplication.java
+++ customer-api/src/main/java/com/carlease/customer/api/CustomerApiApplication.java
@@ -1,12 +1,8 @@
 package com.carlease.customer.api;
 
-import com.carlease.customer.entity.Customer;
-import com.carlease.customer.repo.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -16,14 +12,5 @@ public class CustomerApiApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CustomerApiApplication.class, args);
-  }
-
-  @Bean
-  public CommandLineRunner demo(CustomerRepository repository) {
-    return (args) -> {
-      Customer one = new Customer();
-      one.setName("Justin");
-      repository.save(one);
-    };
   }
 }
---
+++ scripts/init_customers.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -H "Content-Type: application/json" -d "{ \
+  \"name\":\"Sherlock Holmes\", \
+  \"emailAddress\":\"sherlock@carlease.com\", \
+  \"houseNumber\":\"221b\", \
+  \"street\":\"Baker Street\", \
+  \"place\":\"London\", \
+  \"zipCode\":\"NW1 6XE\", \
+  \"phoneNumber\":\"555-1234\" \
+}" http://localhost/api/customers/
